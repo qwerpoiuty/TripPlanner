6ac499dfa62b233df7bd8c89278384b0394daf53
Samuel Leavin <sam.leavin@att.net>
2015-05-13 17:51:49 -0400

    updated with sass

--- app.js
+++ app.js
@@ -4,23 +4,32 @@ var parser = require("body-parser");
 var path = require("path");
 var swig = require("swig");
 var routes = require("./routes/");
+var sass = require('node-sass-middleware');
 
 var app = express();
 var server = app.listen(1234);
 
 
-swig.setDefaults({chache:false});
+swig.setDefaults({cache:false});
 app.engine("html", swig.renderFile);
 app.set("view engine", "html");
 app.set("views", path.join(__dirname,"/views"));
+app.set('view cache', false);
 
 app.use(logger('dev'));
 app.use(parser.json());
 app.use(parser.urlencoded({extended: false}));
 
+app.use(
+  sass({
+    src: path.join(__dirname, '/assets'), //where the sass files are 
+    dest: path.join(__dirname, '/public'), //where css should go
+    debug: true
+  })
+);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')))
-
 app.use('/',routes);
 
 app.use(function(req, res, next) {
